a98852ce85dfcda89993f70bcb3093bdb3404e3a
Abhishek Singh <abhisheksinghxix@gmail.com>
2022-02-01 00:06:23 +0530

    conditional rendering

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,43 @@
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [isLoading, setLoading] = useState(true);
+  const [isError, setError] = useState(false);
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((user) => {
+        setLoading(false);
+        setError(false);
+        setUser(user);
+      })
+      .catch((err) => {
+        setError(true);
+      });
+  }, []);
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+  if (isError) {
+    return <h2>ERROR</h2>;
+  }
+  return (
+    <ul className='users'>
+      <li className='user'>
+        {console.log(user)}
+        <img src={user.avatar_url} alt='' />
+        <h3>{user.name}</h3> <br />
+        <a
+          href='https://github.com/QuincyLarson'
+          target='_blank'
+          rel='noopener noreferrer'>
+          Profile
+        </a>
+      </li>
+    </ul>
+  );
 };
 
 export default MultipleReturns;
